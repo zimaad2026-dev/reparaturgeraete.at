55af7f928a3a3925731d53629df5fb82021b3c19
zimaad2026-dev <zima.ad2026@gmail.com>
2026-03-17 09:53:51 +0100

    Update contact.ts

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,6 +1,6 @@
-export const SERVICE_PHONE = "+436764531979";
+export const SERVICE_PHONE = "+436604576353";
 
-export const SERVICE_PHONE_HREF = "tel:+436764531979";
+export const SERVICE_PHONE_HREF = "tel:+436604576353";
 
 export const SERVICE_EMAIL = "office@reparaturgeraete.at";
 
